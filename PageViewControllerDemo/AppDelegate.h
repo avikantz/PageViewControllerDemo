9abcb9d8390233a627e82dd8eb1cf4d32e65462e
avikantz <avikantsainidbz@gmail.com>
2015-03-27 00:45:09 +0530

    Initial Commit

---
+++ PageViewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageViewControllerDemo
+//
+//  Created by Avikant Saini on 3/27/15.
+//  Copyright (c) 2015 avikantz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
